24d0d63cbea45b9004dc21685c357b6d369ebc48
michael mapesa <michaelmapesa93@gmail.com>
2022-03-09 18:23:18 +0300

    add and set email.py

---
+++ app/email.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_mail import Mail, Message
+import os
+
+from app import app
+
+mail_setting = {
+     "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ.get('SENDER_EMAIL'),
+    "MAIL_PASSWORD": os.environ.get('EMAIL_PWD')
+    
+}
+app.config.update(mail_setting)
+mail = Mail(app)
